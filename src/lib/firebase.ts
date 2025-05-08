cbeaf5ee4ae97bb457c1fd6b44cd6e4d04836773
Karthi Sambath <114901598+karthis17@users.noreply.github.com>
2025-05-08 23:01:44 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,10 +10,10 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD_YHVYPlJ4I7pB2xlDbLHV-7d6KLmKcww",
-    authDomain: "byond-7a6d7.firebaseapp.com",
-    projectId: "byond-7a6d7",
-    storageBucket: "byond-7a6d7.firebasestorage.app",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
     messagingSenderId: "887883808399",
     appId: "1:887883808399:web:f23268f11ef8d85acfbafa",
     measurementId: "G-EGYJY23FST"
